b761ea5bc53afba117d86de3d253004458eacd69
⚜️ ╰‿╯ MR Pabasara Malinda  ད★࿐ <142970254+CyberPabuZa0@users.noreply.github.com>
2025-01-02 19:25:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -24,7 +24,7 @@ function convertToBool(text, fault = 'true') {
 
 //--------------------------Mr Frank--------------------------------------
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || "Enter Your SubZero Session ID",
+    SESSION_ID: process.env.SESSION_ID || "qRQTjaLa#59KzzFkja1xrN6tUBRvA8KPoBUsKh1qNHsDYcJ4iXrk",
     CAPTION: process.env.CAPTION || "*©  Gᴇɴᴇʀᴀᴛᴇᴅ Bʏ SᴜʙZᴇʀᴏ MD*",
     ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true",
     AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
